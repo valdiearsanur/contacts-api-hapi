23512a2e118a0d57753d18f167e1c727b9b5ee08
Muhammad Valdie Arsanur <muhammad.arsanur@go-jek.com>
2022-03-07 07:33:33 +0700

    add GET /contacts

--- index-native.js
+++ index-native.js
@@ -9,6 +9,21 @@ const init = async () => {
   const server = http.createServer((request, response) => {
     response.setHeader('Content-Type', 'application/json')
 
+    const { url, method } = request
+
+    // GET /contacts
+    if (url === '/contacts' && method === 'GET') {
+      let contacts = [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'john@example.com',
+          phone: '1234567890'
+        }
+      ]
+      return response.end(JSON.stringify(contacts))
+    }
+
     // default handler
     response.statusCode = 404
     return response.end(JSON.stringify({ message: 'URL not found' }))
